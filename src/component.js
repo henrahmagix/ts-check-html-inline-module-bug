ea8ee7d45d861165ec2852b7aea3e5f81eb67eb4
Henry Blyth <blyth.henry@gmail.com>
2020-04-12 21:25:35 +0100

    Add jsconfig and newlines

--- src/component.js
+++ src/component.js
@@ -1,3 +1,3 @@
 import { anything } from './module.js';
-// Correct: foo.anything autocompletes nicely
+// Correct: anything is available, and Go To Definition goes to src/module.js
 alert('anything in .js: ' + anything); // true
